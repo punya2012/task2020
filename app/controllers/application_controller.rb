5b541fa767acee1f7e8f8f99aacdf8a3de8304df
EC2 Default User <ec2-user@ip-172-31-44-83.us-east-2.compute.internal>
2020-01-17 09:26:25 +0000

    C6 OK

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def set_user
+      @user = User.find(params[:user_id])
+  end
+    
 end
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,41 @@
+class TasksController < ApplicationController
+  before_action :set_user
+  
+  def index
+    @tasks = @user.tasks
+  end
+  
+  def show
+  end
+
+  def new
+   @task = Task.new
+  end
+  
+  def create
+    @task = @user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました"
+      redirect_to user_tasks_url @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  private
+    
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+    
+    def task_params
+      params.require(:task).permit(:name, :description, :user_id)
+    end
+      
+   
+  
+end
