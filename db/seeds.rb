13e5339b0ac13375d3e3f2c111bb508c9195f482
EC2 Default User <ec2-user@ip-172-31-44-83.us-east-2.compute.internal>
2020-02-02 01:27:54 +0000

    user-destroy OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,8 @@
 User.create!( name: "管理者",
               email: "sample@email.com",
               password: "password",
-              password_confirmation: "password")
+              password_confirmation: "password",
+              admin: true)
 
 
 100.times do |n|
